d228a732d1455fb8df4e2a189d79bff869a60699
giovanemachado <giovanemachado3@gmail.com>
2023-03-22 20:49:26 -0300

    add some animations, fighting spawns

--- src/player/player.gd
+++ src/player/player.gd
@@ -1,20 +1,21 @@
 extends Node2D
 
+# Tree hits
+signal hit(cuttingAt)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_pressed("cut_right"):
-		print("cut right pressed");
+		hitTheTree(cutSide.CUT_RIGHT)
 	if Input.is_action_pressed("cut_left"):
-		print("cut left pressed")
+		hitTheTree(cutSide.CUT_LEFT)
 
-	# if velocity.length() > 0:
-	#     velocity = velocity.normalized() * speed
-	#     $AnimatedSprite2D.play()
-	# else:
-	#     $AnimatedSprite2D.stop()
+# Possible sides to cut the tree down
+enum cutSide {CUT_RIGHT, CUT_LEFT}
+
+func hitTheTree(cuttingAt):
+	hit.emit(cuttingAt)
--- src/tree/tree.gd
+++ src/tree/tree.gd
@@ -1,11 +1,21 @@
 extends Node2D
 
+@export var TreeTuck: PackedScene
+@export var TreeSpawn: Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+#	var treeTuck = TreeTuck.instantiate()
+#	add_child(treeTuck)
+#	treeTuck.position = $TreePosition.position
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_player_hit(cuttingAt):
+	print("cutting at ", cuttingAt)
+	
