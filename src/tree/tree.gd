3207a3010326727dd589d4b32aa3afa545ecf4dc
giovanemachado <giovanemachado3@gmail.com>
2023-03-24 11:00:16 -0300

    animations almost working, arrays fixed, in parts

--- src/tree/tree.gd
+++ src/tree/tree.gd
@@ -1,26 +1,58 @@
 extends Node2D
 
+
 @export var TreeTuck: PackedScene
 @export var TreeSpawn: Node2D
 
-@export var CurrentTucks : Array[Node]
+@export var CurrentTucksNps: Array[NodePath]
+@onready var CurrentTucks = CurrentTucksNps.map(get_node) 
+
+var canHit: bool = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var treeTuck = TreeTuck.instantiate()
-	add_child(treeTuck)
-	treeTuck.position = $TreePosition.position
-	CurrentTucks.append(treeTuck)
 	pass
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-
 func _on_player_hit(cuttingAt):
-	var treeTuck = TreeTuck.instantiate()
-	add_child(treeTuck)
-	treeTuck.position = $TreePosition.position
-	CurrentTucks.append(treeTuck)
+	if !canHit:
+		return
+		
+	canHit = false
+	
+	var hittedTuck = CurrentTucks[1]
+	var branchSide = hittedTuck.get("BranchSide")
+	
+	if branchSide == cuttingAt:
+		print("bad")
+		return
+	
+	print("good")
+	
+	var i = 0
+	for tuck in CurrentTucks:
+		if i == 0:
+			tuck.Hitted()
+			i += 1
+			continue
+		
+		tuck.MoveDown()
+		i += 1
+	
+	CurrentTucks.remove_at(0)
+	
+	for tuck in CurrentTucks:
+		print(tuck.name)
+	
+	var newTreeTuck = TreeTuck.instantiate()
+	add_child(newTreeTuck)
+	newTreeTuck.position = $TreePosition.position
+	CurrentTucks.append(newTreeTuck)
+
+
+func _on_tree_tuck__on_animation_player_animation_finished_tree_tuck(anim_name):
+	print("can hit again") # code dies here
+	canHit = true
